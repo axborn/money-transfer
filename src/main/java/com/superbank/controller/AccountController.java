d039c3e0cc54ade8b52942e5f47b0f4ea88aa0b9
ignas <ignas@ignas.ignas>
2019-12-07 23:06:15 +0200

    basic implementation

---
+++ src/main/java/com/superbank/controller/AccountController.java
@@ -0,0 +1,13 @@
+package com.superbank.controller;
+
+public class AccountController {
+
+	public AccountController () {
+		
+	}
+	
+	public String getAccount(String params) {
+		return "Account " + params;
+	}
+
+}
